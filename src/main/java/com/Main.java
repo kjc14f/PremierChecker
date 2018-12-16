57c6fddf0bf06cc241864c7e16fa88b522058f49
Kyle Chard <kjc2g14@soton.ac.uk>
2018-12-16 20:04:23 +0000

    Dynamic screen size

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -13,34 +15,15 @@ public class Main extends Application {
     }
 
     public Main() {
-//        getTeams();
-//        matchFixtures(getFixtures());
-//
-//        for (int i = 1; i <= GAMEDAYS; i ++) {
-//
-//            calculatePlays(i);
-//            calculatePlaces();
-//
-//            System.out.println(i + " ***** GAMEWEEK " + gameweeks +  "  ---  " + formatter.format(teams.get(0).getFixtures().get(i).getDeadlineTime()) + " *****");
-//            System.out.format("%24s%13s%13s%13s%13s%13s%13s%13s%13s\n", "NAME", "STRENGTH", "DIFFICULTY", "DIF DIC", "NET PLACE",
-//                    "H ATTACK", "H DEFENSE", "A ATTACK", "A DEFENSE");
-//            System.out.format("%54s\n", "-----------------------------------------------------------------------------------------------------------------------");
-//            for (com.Model.Team team : teams) {
-//                System.out.format("%24s%13d%13d%13d%13d%13d%13d%13d%13d\n", team.getName(), team.getStrengthTotal(),
-//                        team.getDifficultyTotal(), team.getDifferenceDifficulty(), team.getOutOfPlace(), team.getHomeAttackStrength(),
-//                        team.getHomeDefenseStrength(), team.getAwayAttackStrength(), team.getAwayDefenseStrength());
-//            }
-//            System.out.println("\n\n");
-//            gameweeks++;
-//        }
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("PremierChecker.fxml"));
-        //new Controller().setup();
         primaryStage.setTitle("Premier Checker");
-        primaryStage.setScene(new Scene(root, 1900, 900));
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        System.out.println(screenSize.getHeight());
+        primaryStage.setScene(new Scene(root, screenSize.getWidth() - 50, screenSize.getHeight() - 100));
         primaryStage.show();
     }
 
